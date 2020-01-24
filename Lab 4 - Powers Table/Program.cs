efea43b419a4e49e2f024a8b3240e6c01d1e11d0
kyle <kyleghafari@gmail.com>
2020-01-23 22:03:46 -0500

    Refactored; Sent the math-y bits to a method, used string formatting to get nicer output

--- Lab 4 - Powers Table/Program.cs
+++ Lab 4 - Powers Table/Program.cs
@@ -6,13 +6,6 @@ namespace Lab_4___Powers_Table
     {
         static void Main(string[] args)
         {
-            //powers table
-
-            //input: prompts user for integer
-
-            //proccessing: goes from 1 to userInput, returns square and cube of each
-
-            //output: pretty table of 1)the number, 2)square, 3)cube
 
             Console.WriteLine("Welcome to Kyle's square and cube program");
             Console.WriteLine("----------------------------------------\n");
@@ -22,12 +15,25 @@ namespace Lab_4___Powers_Table
             while (keepGoing == "y")
             {
                 Console.WriteLine("How high would you like to go?");
-                int userInput = int.Parse(Console.ReadLine());
+                string userInput = Console.ReadLine();
+                int inputAsInt;
+                bool isInt = int.TryParse(userInput, out inputAsInt);
+                while (isInt == false)
+                {
+                    Console.WriteLine("Invalid entry. Please enter a whole number.");
+                    userInput = Console.ReadLine();
+                    isInt = int.TryParse(userInput, out inputAsInt);
+                }
+
+                //print table heading
+                Console.WriteLine("==================================");
+                Console.WriteLine("Number   |   Square   |   Cube   |");
+                Console.WriteLine("==================================");
 
                 //for each number, up to and inclusive of what the user entered, print the number, the square and the cube
-                for (int i = 0; i < userInput; i++)
+                for (int i = 1; i <= inputAsInt; i++)
                 {
-                    Console.WriteLine($"Number: {i}     Square: {i * i}     Cube:{i * i * i}");
+                    ReturnPowers(i);
                 }
 
                 //prompt to continue, rejects anything except "y" and "n"
@@ -42,6 +48,14 @@ namespace Lab_4___Powers_Table
             }
 
             Console.WriteLine("Thanks for stoping by!");
+        }
+
+        //takes input, produces square and cube of input, and writes result to console
+        public static void ReturnPowers(double input)
+        {
+            double square = Math.Pow(input, 2);
+            double cube = Math.Pow(input, 3);
+            Console.WriteLine(String.Format(" {0, -7} | {1, -10} | {2, -10}", input, square, cube));
         }
 
     }
